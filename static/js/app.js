29cfb07eb16332e736ab3abc7ae52330d1b81880
Erik Hernandez <erik.hernandez.enriquez@gmail.com>
2021-04-24 13:55:35 -0500

    working filter

--- static/js/app.js
+++ static/js/app.js
@@ -2,19 +2,28 @@
 var tableData = data;
 
 // YOUR CODE HERE!
-// Using the UFO dataset provided in the form of an array of JavaScript objects, append a table to the web page and add new rows of data for each UFO Sighting 
+// Viewing the available data fromt he data.js
+// console.log(tableData);
 
-// Get a reference to the table body
+// Get references
 var tbody = d3.select("tbody");
+// Select the button
+var button = d3.select("#filter-btn");
+// Select the form
+var form = d3.select('form');
+// Select the date
+var inputElement = d3.select('#datetime');
 
-// Console.log the sightings data from data.js
-console.log(data);
+var columns = ["datetime", "city", "state", "country", "shape", "durationMinutes", "comments"]
+
+// Console.log the data_filtereds data from data.js
+console.log(tableData);
 
 // Use d3 to update each cell's text with report values
-data.forEach(function(sighting) {
-  console.log(sighting);
+data.forEach(function(data_filtered) {
+  console.log(data_filtered);
   var row = tbody.append("tr");
-  Object.entries(sighting).forEach(function([key, value]) {
+  Object.entries(data_filtered).forEach(function([key, value]) {
     console.log(key, value);
 
     // Append a cell to the row for each value
@@ -25,51 +34,40 @@ data.forEach(function(sighting) {
   });
 });
 
-// Use a date form in your HTML document and write JavaScript code that will search through the date/time column to find rows that match
 
-// Assign the data from data.js to a descriptive variable
-var tableData = data;
 
-// Select the button
-var button = d3.select("#filter-btn");
-
-// Select the form
-var form = d3.select('form');
 
 // Create event handlers
 button.on("click", submitForm);
 form.on("submit", submitForm);
 
-// Complete the event handler function for the form
+// Form Function - filter data when submit and button click
 function submitForm() {
 
   // Prevent the page from refreshing
   d3.event.preventDefault();
 
-  // Select the input element and get the raw HTML node
-  var inputElement = d3.select('#datetime');
-
-  // Get the value property of the input element
+  // Get the value from the input
   var inputValue = inputElement.property('value');
 
   console.log(inputValue);
   console.log(tableData);
 
-  var filteredData = tableData.filter(sighting => sighting.datetime === inputValue);
-
+  var filteredData = tableData.filter(data_filtered => data_filtered.datetime === inputValue);
   console.log(filteredData);
 
   // Clear current table body
-  d3.select('tbody').text('');
+  tbody.html("");
+  
 
   // Get a reference to the table body and save to new variable
   var filterTbody = d3.select("tbody");
 
   // Create table pulling information matching the filter
-  filteredData.forEach(function(sighting) {
-  console.log(sighting);
+  filteredData.forEach(function(data_filtered) {
+  console.log(data_filtered);
   var row = filterTbody.append("tr");
-  Object.entries(sighting).forEach(function([key, value]) {
+  Object.entries(data_filtered).forEach(function([key, value]) {
     console.log(key, value);
 
     var cell = row.append("td");
